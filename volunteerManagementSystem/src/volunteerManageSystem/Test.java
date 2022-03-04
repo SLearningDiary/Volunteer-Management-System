2e43d0e9bf78c827cd3c164fa05bb03a69858097
Song <63572034+SLearningDiary@users.noreply.github.com>
2022-03-04 17:44:21 +0800

    Add files via upload

---
+++ volunteerManagementSystem/src/volunteerManageSystem/Test.java
@@ -0,0 +1,8 @@
+package volunteerManageSystem;
+
+public class Test {
+    public static void main(String[] args) {
+        CustomerView cView = new CustomerView();
+        cView.enterMainMenu();
+    }
+}
